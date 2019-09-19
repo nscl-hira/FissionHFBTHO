ee1235322f2c303a8575a4a9443799ab47106b35
Adam Anthony <antho121@msu.edu>
2019-09-19 17:14:05 -0400

    Add env.sh

---
+++ env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Load all of the required modules to run the code
+module purge
+module load intel
+module load powertools
+
+#Set the directories for HFBTHO
+export HFBTHODIR="/home/antho121/HFBTHO/newBuild/hfbtho-master/src/hfbtho"
